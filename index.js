13ce5c2ed10170896f96412b5c1fe20eca12076e
Romulus <romulus.senter@yahoo.com>
2018-03-14 18:37:13 -0400

    Created a product router and wired up the router up to the main server application

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const serverApp = express();
 
 const PORT = process.env.PORT || 5000; //necessary for HEroku deployment
+
+//Routers
+const productRouter = require('./routers/products');
+
+serverApp.use(productRouter); //register the products with the router
+
 serverApp.get('/', (req,res) =>{
     res.send('stufff');
-})
+});
 
 serverApp.listen(PORT, () =>{
     console.log(`Now listening on this port ${PORT}`);
-})
+});
 
