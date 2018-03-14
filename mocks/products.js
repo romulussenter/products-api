03ced3922720280979da8b21ba07b17380d56290
Romulus <romulus.senter@yahoo.com>
2018-03-14 19:29:56 -0400

    Created mock product data and servered that data on the browser.

---
+++ mocks/products.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        _id: 'fasdjfajsdfh',
+        name: 'Javelin',
+        price: 500,
+        imgSrc: 'http://via.palceholder.com/250x250',
+        created: new Date()
+    },
+    {
+        _id: 'fakjdmjjrjjr',
+        name: 'rug',
+        price: 110,
+        imgSrc: 'http://via.palceholder.com/250x250',
+        created: new Date()
+   
+    },
+    {
+        _id: 'fasaefafeh',
+        name: 'Snail',
+        price: 20,
+        imgSrc: 'http://via.palceholder.com/250x250',
+        created: new Date()
+    
+    }
+];
+
+
+
+module.exports = products;
