6f458d18f8ce44b80d8a027d0e7045399b565c3a
Romulus <romulus.senter@yahoo.com>
2018-03-14 19:47:56 -0400

    created helper fuction to orginize an array of ojects

--- routers/products.js
+++ routers/products.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const mockProducts =require('../mocks/products');
-
+const productArrToObj = (arrayOfProducts) => {
+    //creat an accumlator object
+    const accumlator = {};
+    //for Each priduct in arrayOfProducts
+    arrayOfProducts.forEach(product => {
+        const id = product._id;
+        const copy = {...product}
+        delete copy.id;
+        accumlator[id] = copy;
+    });
+        //grab the id
+        //delete the _id internal to the oject
+        //set the id value in the accumulator object equal to product
+        //return accumulator
+        return accumlator;
+}
 router.get('/products', (req, res) => {
     res.status(200).json({
-        products: mockProducts
+        products: productArrToObj(mockProducts)
     })
 });
 
